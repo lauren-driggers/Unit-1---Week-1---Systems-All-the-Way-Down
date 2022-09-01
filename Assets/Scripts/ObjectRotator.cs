aa48feebe5ede4c7c185178c5815c537849012c4
lauren-driggers <lmd3ttv@virginia.edu>
2022-09-01 14:28:34 -0400

    Adding moons and pool

--- Assets/Scripts/ObjectRotator.cs
+++ Assets/Scripts/ObjectRotator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ObjectRotator : MonoBehaviour
 {
-    float orbitSpeed = .1f;
+    float orbitSpeed = .2f;
 
     // Start is called before the first frame update
     void Start()
